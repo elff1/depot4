90b9067d46cbf961c2bfe8255139123d6b0b6edb
elff1 <elff1@21cn.com>
2013-12-11 22:17:36 +0800

    generate scaffold Cart and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131208162602) do
+ActiveRecord::Schema.define(version: 20131211141556) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
