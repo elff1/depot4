b722bd773c318987438bc754fec8c62b590fdb36
elff1 <elff1@21cn.com>
2013-12-15 15:12:00 +0800

    combine existed ListItems in DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215062851) do
+ActiveRecord::Schema.define(version: 20131215064219) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
